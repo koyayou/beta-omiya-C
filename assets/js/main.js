6652e8e2acf94beea468bf0905617059d0d79016
honjo <71478481+karui38kota@users.noreply.github.com>
2021-09-06 11:12:44 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,52 +14,53 @@ const  app  =  Vue 。createApp （{
     戻り値 {
       / *解答
       * 元。QC2-3を配列しますはステージ2の配列に解応答をします。
-      * q3： 'すて3',
+      * q3： 'おおお'、
       * /
       correctAnswer：{
-        stage1：{
-          q1：'サンダー' 
-          q2:'ファイヤー'
-          q3:'ワォーター'
-        } 
-        stage2：{
-          q1：'オールデリート' 
-        } 
-        stage3：{
-          q1：'すて3' 
-        }　　
+        ステージ1：{
+          q1：'あああ' 、
+        } 、
+        ステージ2：{
+          q1：'いいい' 、
+          // q2： 'えええ'、
+          // q3： 'おおお'
+        } 、
+        ステージ3：{
+          q1：'ううう' 、
+          // q2： 'かかか'、
+          // q3： 'ききき'、
+        }
       } 、
-
       / *ここの問題が正解かか
       * 元。配列2-3を配列しますは配列に偽り配列します。
       * /
       答え：{
         ステージ1：[
-          false、 1-1
-          false、　　1-2
-          false、　　1-3
+          false 、
         ] 、
         ステージ2：[
-          false 、
+          false 、 // 2-1
+          // false、// 2-2
+          // false、// 2-3
         ] 、
         ステージ3：[
-          false 、
+          false 、 // 3-1
+          // false、// 3-2
+          // false、// 3-3
         ]
       } 、
-
       / *ステージの問題があり正解かか* /
       クリア：{
-        stage1：true 、
-        stage2：true 、
+        stage1：false 、
+        ステージ2：false 、
         stage3：false 、
       } 、
-
       / *実行のクラスターを表示するか
       *最終キャンペーンはオプションをするので設定します。
       * /
       次へ：{
         stage1：false 、
-        stage2：false 、
+        ステージ2：false 、
       } 、
     }
   } 、
@@ -88,7 +89,6 @@ const  app  =  Vue 。createApp （{
     } 、
   }
 } ）
-
 / *解答回答別のバイナリ* /
 アプリ。コンポーネント（'answer-input' 、 {
   小道具：[ '正しい' ] 、
@@ -96,31 +96,12 @@ const  app  =  Vue 。createApp （{
     戻り値 {
       / *積ボタンあり表示呪文* /
       okMes​​sage：'正解！' 、
-      ngMessage：'その外はてようだぞ！？' 、
+      ngMessage：'エラー！' 、
       メッセージ：'' 、
       inputAnswer：'' 、
     }
-  } 、
-  テンプレート：`
-    <div class = "answer__container">
-      <div class = "answer">
-        <input type = "text" v-model = "inputAnswer" placeholder = "ここに表を入力テトラ">
-      </ div>
-      <p v-if = "message === ngMessage" class = "err-message"> {{メッセージ}} </ p>
-      <button v-on：click = "judgement（inputAnswer）">送信</ button>
-      <p v-if = "message === okMes​​sage" class = "err-message"> {{メッセージ}} </ p>
-    </ div> ` 、
-  メソッド：{
-    判断（答え） {
-      もし（答え === 本。正しい） {  //入力値が解答と一致する場合
-        これ。メッセージ = これ。okMes​​sage ;
-        これ。$ emit （'answerInput' 、 true ）;
-      }  else  {  //一致します
-        これ。メッセージ = これ。ngMessage ; 
-        これ。$ emit （'answerInput' 、 false ）;
-      }
-    } 、
+@@ -127,4 +127,4 @@ app.component（ 'answer-input'、{
   }
 } ）
 
-アプリ。マウント（'#stage' ）
+アプリ。マウント（'#stage' ） 
