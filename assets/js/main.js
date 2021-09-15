c4b66a491962c66dad1928622fd943a362645bec
honjo <71478481+karui38kota@users.noreply.github.com>
2021-09-15 22:57:02 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ const app = Vue.createApp({
     return {
       correctAnswer: {
         stage1: {
-          q1: 'ファイヤー','ファイアー'
+          q1: 'ファイヤー',
         },
         stage2: {
           q1: 'ウォーター',
